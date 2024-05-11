0a8b58af5fb6a63f022edcb356c48f1a5f1b1be3
LOUK342 <kimjaeho342@gmail.com>
2024-05-12 01:48:01 +0900

    1

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ const app = express();
 const port = 3000;
 let temp = 0;
 let hm = 0;
+let UUID = "0";
+
+const wss = new WebSocket.Server({ port: 3030 });
 
 app.get("/data", function (req, res) {
   res.json({
+    UUID: UUID,
     temp: temp,
     hm: hm,
   });
 });
 
-const server = app.listen(port, function () {
+app.listen(port, function () {
   console.log("Server running at " + port);
 });
-
-const wss = new WebSocket.Server({ port: 3030 });
 
 wss.on("connection", (ws) => {
   console.log("WebSocket client connected");
@@ -28,7 +30,8 @@ wss.on("connection", (ws) => {
       const jsonData = JSON.parse(data);
       temp = jsonData.temp;
       hm = jsonData.hm;
-      console.log("temp : ", temp, " / hm : ", hm);
+      UUID = jsonData.uuid;
+      console.log("UUID : ", UUID, " / temp : ", temp, " / hm : ", hm);
     } catch (error) {
       console.error("Error parsing JSON data:", error);
     }
