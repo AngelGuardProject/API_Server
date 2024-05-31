186b35a81be10116ee85a496cb0edf5f1ac31967
LOUK342 <kimjaeho342@gmail.com>
2024-05-31 23:50:18 +0900

    MIC 및 온습도 작동

---
+++ MIC.ino
@@ -0,0 +1,109 @@
+//https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
+//위 주소 추가해서 ESP32S3 Dev Module 설정 후
+//Tool 들어가서 Flash 16MB , Event & arduino 모두 core 1 , PSRAM은 OPI로 해주면 됨
+//보드매니저에서 esp32 2.0.17로 내려야 함
+#include <driver/i2s.h>
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+
+#define I2S_SD 1
+#define I2S_WS 4
+#define I2S_SCK 5
+#define I2S_PORT I2S_NUM_0
+
+#define bufferCnt 10
+#define bufferLen 1024
+int16_t sBuffer[bufferLen];
+
+const char* ssid = "Louk-2.4G";
+const char* password = "#i2ScO8unSF7";
+
+
+using namespace websockets;
+WebsocketsClient client;
+bool isWebSocketConnected;
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+  if (event == WebsocketsEvent::ConnectionOpened) {
+    Serial.println("Connnection Opened");
+    isWebSocketConnected = true;
+  } else if (event == WebsocketsEvent::ConnectionClosed) {
+    Serial.println("Connnection Closed");
+    isWebSocketConnected = false;
+  } else if (event == WebsocketsEvent::GotPing) {
+    Serial.println("Got a Ping!");
+  } else if (event == WebsocketsEvent::GotPong) {
+    Serial.println("Got a Pong!");
+  }
+}
+
+void i2s_install() {
+  const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = bufferCnt,
+    .dma_buf_len = bufferLen,
+    .use_apll = false
+  };
+
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+
+void i2s_setpin() {
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD
+  };
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  connectWSServer();
+  xTaskCreatePinnedToCore(micTask, "micTask", 10000, NULL, 1, NULL, 1);
+}
+
+void loop() {
+}
+
+void connectWiFi() {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void connectWSServer() {
+  client.onEvent(onEventsCallback);
+  while (!client.connect("ws://louk342.iptime.org:3020")) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Websocket Connected!");
+}
+
+
+void micTask(void* parameter) {
+  i2s_install();
+  i2s_setpin();
+  i2s_start(I2S_PORT);
+
+  size_t bytesIn = 0;
+  while (true) {
+    esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+    if (result == ESP_OK && isWebSocketConnected) client.sendBinary((const char*)sBuffer, bytesIn);
+    else Serial.println("I2S read error or WebSocket not connected");
+  }
+}
--- server.js
+++ server.js
@@ -1,39 +1,52 @@
 const express = require("express");
 const WebSocket = require("ws");
+const path = require("path");
 const app = express();
 
-const port = 3000;
+const port = 3010;
 let temp = 0;
 let hm = 0;
 let UUID = "0";
 
-const wss = new WebSocket.Server({ port: 3030 });
+let connectedClients = [];
 
+const wss = new WebSocket.Server({ port: 3030 });
+const mics = new WebSocket.Server({ port: 3020 });
+
+app.use("/image", express.static("image"));
+app.use("/js", express.static("js"));
+app.get("/audio", (req, res) => res.sendFile(path.resolve(__dirname, "./audio_client.html")));
 app.get("/data", function (req, res) {
   res.json({
-    UUID: UUID,
     temp: temp,
     hm: hm,
   });
 });
 
-app.listen(port, function () {
-  console.log("Server running at " + port);
-});
-
 wss.on("connection", (ws) => {
-  console.log("WebSocket client connected");
-
+  console.log("wws connected");
   ws.on("message", function incoming(data) {
     console.log("Received message:", data);
     try {
       const jsonData = JSON.parse(data);
       temp = jsonData.temp;
       hm = jsonData.hm;
-      UUID = jsonData.uuid;
-      console.log("UUID : ", UUID, " / temp : ", temp, " / hm : ", hm);
+        console.log("temp : ",temp," / hm : ",hm);
     } catch (error) {
       console.error("Error parsing JSON data:", error);
     }
   });
 });
+
+mics.on("connection", (ws, req) => {
+  console.log("MIC server connected");
+  connectedClients.push(ws);
+  ws.on("message", (data) => {
+    connectedClients.forEach((ws, i) => {
+      if (ws.readyState === ws.OPEN) ws.send(data);
+      else connectedClients.splice(i, 1);
+    });
+  });
+});
+
+app.listen(port, function () { console.log("Server running at " + port);});
