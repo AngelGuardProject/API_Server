8c26253b33330d11b15f02c67dc8d0081a9628bd
LOUK342 <kimjaeho342@gmail.com>
2024-06-08 23:01:54 +0900

    UUID test

--- _inoFiles/TempHm.ino
+++ _inoFiles/TempHm.ino
@@ -1,3 +1,7 @@
+// DHT sensor library / by Adafruit
+// ESP8266 and ESP32 OLED driver for SSD1306 displays / by THingPulse
+// ArduinoJson / by Benoit Blanchon
+// ArduinoWebsockets / by Gll Malmon
 #include <WiFi.h>
 #include <DHT.h>
 #include <Wire.h>
@@ -13,94 +17,94 @@ WebsocketsClient client;
 
 void setup()
 {
-    Serial.begin(115200);
-    pinMode(2, OUTPUT);
-    // Wi-Fi
-    WiFi.begin("Louk-2.4G", "#i2ScO8unSF7");
-    Serial.print("Wi-Fi Connecting");
-    for (int i = 0; i < 200 && WiFi.status() != WL_CONNECTED; i++)
-    {
-        Serial.print(".");
-        delay(50);
-    }
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.println("Can't connect Wifi until 10 secends");
-        return;
-    }
-    Serial.println("");
-    Serial.println("Wi-Fi Connected");
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  // Wi-Fi
+  WiFi.begin("Louk-2.4G", "#i2ScO8unSF7");
+  Serial.print("Wi-Fi Connecting");
+  for (unsigned char i = 0; i < 200 && WiFi.status() != WL_CONNECTED; i++)
+  {
+    Serial.print(".");
+    delay(50);
+  }
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Can't connect Wifi until 10 secends");
+    return;
+  }
+  Serial.println("");
+  Serial.println("Wi-Fi Connected");
 
-    dht.begin();
+  dht.begin();
 
-    display.init();
-    display.setFont(ArialMT_Plain_16);
-    /*
-    // EEPROM 초기화
-    EEPROM.begin(512);
-    // UUID 읽기
-    String storedUUID = readUUID();
-    Serial.println("Saved UUID: " + storedUUID);
-    // 저장된 UUID가 없다면 새로운 UUID 생성 및 저장
-    if (storedUUID == "") {
-      String newUUID = "AG_001";
-      Serial.println("Create new uuid: " + newUUID);
-      saveUUID(newUUID);
-    }
-    */
-    if (client.connect("ws://louk342.iptime.org:3030"))
-        Serial.println("WS Connected");
+  display.init();
+  display.setFont(ArialMT_Plain_16);
+  /*
+  // EEPROM 초기화
+  EEPROM.begin(512);
+  // UUID 읽기
+  String storedUUID = readUUID();
+  Serial.println("Saved UUID: " + storedUUID);
+  // 저장된 UUID가 없다면 새로운 UUID 생성 및 저장
+  if (storedUUID == "") {
+    String newUUID = "AG_001";
+    Serial.println("Create new uuid: " + newUUID);
+    saveUUID(newUUID);
+  }
+  */
+  if (client.connect("ws://louk342.iptime.org:3030"))
+    Serial.println("WS Connected");
 }
 
 void loop()
 {
-    // DHT
-    float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
+  // DHT
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
-    if (isnan(temperature) || isnan(humidity))
-    {
-        Serial.println("DHT Error__");
-        return;
-    }
+  if (isnan(temperature) || isnan(humidity))
+  {
+    Serial.println("DHT Error__");
+    return;
+  }
 
-    // OLED
-    display.clear();
-    display.drawString(0, 0, "Temp : " + String(temperature));
-    display.drawString(0, 16, "Humi : " + String(humidity));
-    display.display();
+  // OLED
+  display.clear();
+  display.drawString(0, 0, "Temp : " + String(temperature));
+  display.drawString(0, 16, "Humi : " + String(humidity));
+  display.display();
 
-    // Data push
-    if (temperature * humidity > 250)
-    {
-        StaticJsonDocument<200> json;
-        json["temp"] = temperature;
-        json["hm"] = humidity;
-        // json["uuid"] = readUUID();
-        //  JSON parsing
-        String push;
-        serializeJson(json, push);
-    }
-
-    // WS
+  // Data push
+  if (temperature * humidity > 250)
+  {
     StaticJsonDocument<200> json;
     json["temp"] = temperature;
     json["hm"] = humidity;
-    // json["uuid"] = readUUID();
+    json["uuid"] = 0;
     //  JSON parsing
-    String data;
-    serializeJson(json, data);
-    client.send(data);
+    String push;
+    serializeJson(json, push);
+  }
 
-    // Blink
-    digitalWrite(2, HIGH);
-    delay(10);
-    digitalWrite(2, LOW);
+  // WS
+  StaticJsonDocument<200> json;
+  json["temp"] = temperature;
+  json["hm"] = humidity;
+  json["uuid"] = 0;
+  //  JSON parsing
+  String data;
+  serializeJson(json, data);
+  client.send(data);
 
-    // Console
-    // Serial.println("Temp : "+String(temperature)+", Humi : "+String(humidity));
+  // Blink
+  digitalWrite(2, HIGH);
+  delay(10);
+  digitalWrite(2, LOW);
 
-    delay(1000);
+  // Console
+  // Serial.println("Temp : "+String(temperature)+", Humi : "+String(humidity));
+
+  delay(1000);
 }
 /*
 void saveUUID(String uuid) {
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const WebSocket = require("ws");
 const path = require("path");
 const app = express();
 
-const port = 3010;
-
-let uuid = 0;
-let temp = 0;
-let hm = 0;
+let dataStore = {};
 
 let connectedClients = [];
 
 const wss = new WebSocket.Server({ port: 3030 });
 const mics = new WebSocket.Server({ port: 3020 });
+const port = 3010;
+app.listen(port, function () {
+  console.log("Server running at " + port);
+});
 
 app.use("/image", express.static("image"));
 app.use("/js", express.static("js"));
@@ -20,16 +20,24 @@ app.get("/audio", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./audio_client.html"))
 );
 app.get("/data", function (req, res) {
-  res.json({
-    temp: temp,
-    hm: hm,
-  });
+  const uuid = req.query.uuid;
+  if (uuid) {
+    // 특정 uuid의 데이터를 반환
+    if (dataStore[uuid]) {
+      res.json(dataStore[uuid]);
+    } else {
+      res.status(404).json({ error: "UUID not found" });
+    }
+  } else {
+    // 모든 uuid의 데이터를 반환
+    res.json(dataStore);
+  }
 });
 
 wss.on("connection", (ws) => {
   console.log("wws connected");
   ws.on("message", function incoming(data) {
-    console.log("Received message:", data);
+    //console.log("Received message:", data);
     try {
       const jsonData = JSON.parse(data);
       uuid = jsonData.uuid;
